da029e43403429ebe0366bb65985368dd0f6333a
luisbeltrancondori <luisbeltrancondoricano@gmail.com>
2022-01-02 19:53:28 -0400

    menu

--- config/adminlte.php
+++ config/adminlte.php
@@ -249,97 +249,81 @@ return [
         [
             'text'        => 'Pacientes',
             'url'         => 'admin/pacientes',
-            'icon'        => 'far fa-fw fa-file',
+            'icon'        => 'fas fa-fw fa-users',
            
         ],
 
         [
-            'text'        => 'laboratorista',
+            'text'        => 'Laboratorista',
             'url'         => 'admin/laboratoristas',
+            'icon'        => 'fas fa-fw fa-user',
             
         ],
         [
-            'text'        => 'medicos',
+            'text'        => 'Médicos',
             'url'         => 'admin/medicos',
+            'icon'        => 'fas fa-fw fa-user',
             
            
         ],
-        ['header' => 'account_settings'],
-        [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-user',
-        ],      
-        [
-            'text'        => 'laboratorista',
-            'url'         => 'admin/laboratoristas',
-            
-        ],
-        [
-            'text'        => 'medicos',
-            'url'         => 'admin/medicos',
-            
-           
-        ],
-        ['header' => 'account_settings'],
-
         [
             'text'        => 'Establecimientos',
             'url'         => 'admin/establecimientos',
-            'icon'        => 'far fa-home',
+            'icon'        => 'fas fa-fw fa-list',
            
         ],
 
 
-        ['header' => 'account_settings'],
+        ['header' => 'CONFIGURACION DE CUENTA'],
         [
-            'text' => 'profile',
+            'text' => 'Perfil',
             'url'  => 'admin/settings',
             'icon' => 'fas fa-fw fa-user',
         ],
         [
-            'text' => 'change_password',
+            'text' => 'Cambiar contraseña',
             'url'  => 'admin/settings',
             'icon' => 'fas fa-fw fa-lock',
         ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
+
+        // [
+        //     'text'    => 'multilevel',
+        //     'icon'    => 'fas fa-fw fa-share',
+        //     'submenu' => [
+        //         [
+        //             'text' => 'level_one',
+        //             'url'  => '#',
+        //         ],
+        //         [
+        //             'text'    => 'level_one',
+        //             'url'     => '#',
+        //             'submenu' => [
+        //                 [
+        //                     'text' => 'level_two',
+        //                     'url'  => '#',
+        //                 ],
+        //                 [
+        //                     'text'    => 'level_two',
+        //                     'url'     => '#',
+        //                     'submenu' => [
+        //                         [
+        //                             'text' => 'level_three',
+        //                             'url'  => '#',
+        //                         ],
+        //                         [
+        //                             'text' => 'level_three',
+        //                             'url'  => '#',
+        //                         ],
+        //                     ],
+        //                 ],
+        //             ],
+        //         ],
+        //         [
+        //             'text' => 'level_one',
+        //             'url'  => '#',
+        //         ],
+        //     ],
+        // ],
         ['header' => 'labels'],
         [
             'text'       => 'important',
